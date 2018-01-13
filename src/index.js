92cef64e35de114706c74a447768bf217e083c39
Elizabeth Davis <eadskye@gmail.com>
2018-01-13 23:02:46 +0000

    rendered component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 //Create a new component, should produce some HTML
 //Take this component's generated HTML and put it on the page
+
+import React from 'react';
 const App = function(){
-  return <div>Hi!</div>:
+  return <div>Hi!</div>;
 }
+
+React.render(App);
