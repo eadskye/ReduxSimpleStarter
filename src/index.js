0015eaa98b8779dc2555ffaabde01a2b10b81532
Elizabeth Davis <eadskye@gmail.com>
2018-01-13 23:07:36 +0000

    fixed react dom error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 //Take this component's generated HTML and put it on the page
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 const App = function(){
   return <div>Hi!</div>;
 }
 
-React.render(App);
+ReactDOM.render(App);
