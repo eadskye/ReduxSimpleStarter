452b2c2635b1806675481fca9f43c31cc70b901e
Elizabeth Davis <eadskye@gmail.com>
2018-01-14 12:52:05 +0000

    added searchbar input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 //Create a new component, should produce some HTML
 //Take this component's generated HTML and put it on the page
 const API_KEY = 'AIzaSyDmUOjne6DVTKDn2v3Uea8Y_cgipFL0I4Y';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
