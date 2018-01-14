161abd8e58810fc42f90b1e997830ddac194cfd7
Elizabeth Davis <eadskye@gmail.com>
2018-01-14 15:49:18 +0000

    correct video list error

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
+
 const API_KEY = 'AIzaSyDmUOjne6DVTKDn2v3Uea8Y_cgipFL0I4Y';
 
 class App extends Component {
   constructor(props){
     super(props);
-    
+
     this.state = { videos: [] };
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
       this.setState({ videos });
