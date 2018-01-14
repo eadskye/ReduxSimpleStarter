619304c6a64e9d029e7db43c03cbd1f83d0c2a8d
Elizabeth Davis <eadskye@gmail.com>
2018-01-14 14:22:10 +0000

    added youtube search import to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 //Create a new component, should produce some HTML
 //Take this component's generated HTML and put it on the page
 const API_KEY = 'AIzaSyDmUOjne6DVTKDn2v3Uea8Y_cgipFL0I4Y';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
