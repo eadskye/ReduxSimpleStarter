ef8dc043362387eb0c622417ffc7dd14c1010c04
Elizabeth Davis <eadskye@gmail.com>
2018-01-14 12:33:35 +0000

    added youtube api key and installed package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
-
-//Create a new component, should produce some HTML
-//Take this component's generated HTML and put it on the page
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Create a new component, should produce some HTML
+//Take this component's generated HTML and put it on the page
+const API_KEY = 'AIzaSyDmUOjne6DVTKDn2v3Uea8Y_cgipFL0I4Y';
+
 const App = () => {
   return <div>Hi!</div>;
 }
