547a0bd1397774a3508d5851aa39b834d0014ffd
Elizabeth Davis <eadskye@gmail.com>
2018-01-14 14:29:42 +0000

    changed function to class to add state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 'use strict';
-import React from 'react';
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-//Create a new component, should produce some HTML
-//Take this component's generated HTML and put it on the page
+
 const API_KEY = 'AIzaSyDmUOjne6DVTKDn2v3Uea8Y_cgipFL0I4Y';
 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
   console.log(data);
 });
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        </div>
+      );
+    }
 }
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
